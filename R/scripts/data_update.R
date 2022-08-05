64e38479a9eb7ee62d7be41b74dcf3e85705c10d
Tyson <cwtyson@gmail.com>
2022-08-05 14:36:17 +0200

    Update data_update

--- R/scripts/data_update.R
+++ R/scripts/data_update.R
@@ -1,15 +1,16 @@
 suppressMessages(library(tidyverse))
 
 ## Source files
+source(here::here("./R/profile_info.R"))
 source(here::here("./R/functions/process_dets.R"))
 source(here::here("./R/functions/process_nodes.R"))
 source(here::here("./R/functions/summarize_dets.R"))
 source(here::here("./R/functions/summarize_nodes.R"))
 
 ## Process detection data
-process_dets(db_name = "postgres",
-             db_user = "tyson",
-             db_password = "time00",
+process_dets(db_name = db_name,
+             db_user = db_user,
+             db_password = db_password,
              sensor_station_code = NULL,
              tag_log_file = "/Users/tyson/Documents/academia/institutions/WUR/research/eswatini/field/Tags/LifeTags_log21.xlsx",
              node_log_file = "/Users/tyson/Documents/academia/institutions/WUR/research/eswatini/field/Nodes/Node_deployment_log_Feb2022.xlsx",
@@ -17,13 +18,13 @@ process_dets(db_name = "postgres",
              output_folder = "./Eswatini/data/processed")
 
 ## Process detection data
-process_nodes(db_name = "postgres",
-             db_user = "tyson",
-             db_password = "time00",
-             sensor_station_code = NULL,
-             node_log_file = "/Users/tyson/Documents/academia/institutions/WUR/research/eswatini/field/Nodes/Node_deployment_log_Feb2022.xlsx",
-             tz = "Africa/Mbabane",
-             output_folder = "./Eswatini/data/processed")
+process_nodes(db_name = db_name,
+              db_user = db_user,
+              db_password = db_password,
+              sensor_station_code = NULL,
+              node_log_file = "/Users/tyson/Documents/academia/institutions/WUR/research/eswatini/field/Nodes/Node_deployment_log_Feb2022.xlsx",
+              tz = "Africa/Mbabane",
+              output_folder = "./Eswatini/data/processed")
 
 ## Summarize detections and plot
 summarize_dets(dets_data_file = "./Eswatini/data/processed/dets_filtered.Rdata",
