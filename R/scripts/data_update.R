c6f3e9c35c097f19f7b1d60de7e08c14240e2ed7
Tyson <cwtyson@gmail.com>
2022-08-08 12:31:44 +0200

    Modify data update script to include prepare function

--- R/scripts/data_update.R
+++ R/scripts/data_update.R
@@ -6,37 +6,49 @@ source(here::here("./R/functions/process_dets.R"))
 source(here::here("./R/functions/process_nodes.R"))
 source(here::here("./R/functions/summarize_dets.R"))
 source(here::here("./R/functions/summarize_nodes.R"))
+source(here::here("./R/functions/prepare_dets.R"))
 
-## Process detection data
-process_dets(db_name = db_name,
-             db_user = db_user,
-             db_password = db_password,
-             sensor_station_code = NULL,
-             tag_log_file = "/Users/tyson/Documents/academia/institutions/WUR/research/eswatini/field/Tags/LifeTags_log21.xlsx",
-             node_log_file = "/Users/tyson/Documents/academia/institutions/WUR/research/eswatini/field/Nodes/Node_deployment_log_Feb2022.xlsx",
-             tz = "Africa/Mbabane",
-             output_folder = "./Eswatini/data/processed")
+# ## Process detection data
+# process_dets(db_name = db_name,
+#              db_user = db_user,
+#              db_password = db_password,
+#              sensor_station_code = NULL,
+#              tag_log_file = "/Users/tyson/Documents/academia/institutions/WUR/research/eswatini/field/Tags/LifeTags_log21.xlsx",
+#              node_log_file = "/Users/tyson/Documents/academia/institutions/WUR/research/eswatini/field/Nodes/Node_deployment_log_Feb2022.xlsx",
+#              tz = "Africa/Mbabane",
+#              output_folder = "./Eswatini/data/processed")
+# 
+# ## Process detection data
+# process_nodes(db_name = db_name,
+#               db_user = db_user,
+#               db_password = db_password,
+#               sensor_station_code = NULL,
+#               node_log_file = "/Users/tyson/Documents/academia/institutions/WUR/research/eswatini/field/Nodes/Node_deployment_log_Feb2022.xlsx",
+#               tz = "Africa/Mbabane",
+#               output_folder = "./Eswatini/data/processed")
+# 
+# ## Summarize detections and plot
+# summarize_dets(dets_data_file = "./Eswatini/data/processed/dets_filtered.Rdata",
+#                plot_type = "summary",
+#                interval = "day",
+#                summary_folder = "./Eswatini/data_summaries",
+#                plot_folder = "./Eswatini/plots")
+# 
+# ## Summarize detections and plot
+# summarize_nodes(node_data_file = "./Eswatini/data/processed/node_health.Rdata",
+#                 dets_data_file = "./Eswatini/data/processed/dets_filtered.Rdata",
+#                 plot_type = "summary",
+#                 interval = "day",
+#                 summary_folder = "./Eswatini/data_summaries",
+#                 plot_folder = "./Eswatini/plots")
 
-## Process detection data
-process_nodes(db_name = db_name,
-              db_user = db_user,
-              db_password = db_password,
-              sensor_station_code = NULL,
-              node_log_file = "/Users/tyson/Documents/academia/institutions/WUR/research/eswatini/field/Nodes/Node_deployment_log_Feb2022.xlsx",
-              tz = "Africa/Mbabane",
-              output_folder = "./Eswatini/data/processed")
+## Prepare detection data to localize
+prepare_dets(processed_dets_file = "./Eswatini/data/processed/dets_filtered.Rdata",
+             grid_point_file = "./Eswatini/data/field_data/processed_field_data/grid_point_locations.RData",
+             output_folder = "./Eswatini/data/processed/",
+             window = "30 secs",
+             lag = "-15 secs",
+             dist_filter = 200)
 
-## Summarize detections and plot
-summarize_dets(dets_data_file = "./Eswatini/data/processed/dets_filtered.Rdata",
-               plot_type = "summary",
-               interval = "day",
-               summary_folder = "./Eswatini/data_summaries",
-               plot_folder = "./Eswatini/plots")
+## Localize detections
 
-## Summarize detections and plot
-summarize_nodes(node_data_file = "./Eswatini/data/processed/node_health.Rdata",
-                dets_data_file = "./Eswatini/data/processed/dets_filtered.Rdata",
-                plot_type = "summary",
-                interval = "day",
-                summary_folder = "./Eswatini/data_summaries",
-                plot_folder = "./Eswatini/plots")
