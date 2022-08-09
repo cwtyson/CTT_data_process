51523f38d31f2f943dfe0d928fa6c1d02bf6f62c
Tyson <cwtyson@gmail.com>
2022-08-09 11:13:38 +0200

    Edit data_update script

--- R/scripts/data_update.R
+++ R/scripts/data_update.R
@@ -1,47 +1,55 @@
-library(tidyverse, warn.conflicts = FALSE); library(geosphere)
+## Housekeeping
+library(magrittr)
 
-## Source files
+## Set up profile with personal information
 source(here::here("./R/profile_info.R"))
-source(here::here("./R/functions/process_dets.R"))
-source(here::here("./R/functions/process_nodes.R"))
-source(here::here("./R/functions/summarize_dets.R"))
-source(here::here("./R/functions/summarize_nodes.R"))
-source(here::here("./R/functions/prepare_dets.R"))
-source(here::here("./R/functions/localize.R"))
 
-# ## Process detection data
-# process_dets(db_name = db_name,
-#              db_user = db_user,
-#              db_password = db_password,
-#              sensor_station_code = NULL,
-#              tag_log_file = "/Users/tyson/Documents/academia/institutions/WUR/research/eswatini/field/Tags/LifeTags_log21.xlsx",
-#              node_log_file = "/Users/tyson/Documents/academia/institutions/WUR/research/eswatini/field/Nodes/Node_deployment_log_Feb2022.xlsx",
-#              tz = "Africa/Mbabane",
-#              output_folder = "./Eswatini/data/processed")
+## Source functions
+funs <- list.files(here::here("./R/functions"), full.names = TRUE)
+for(fun in funs){
+  source(fun)
+}
+
+
+## Set up new project
+initalize_project(project = "zebby_tracking",
+                  google_drive_folder = "Zebby_tracking_field_data")
+
+## Fetch and update field data
+fetch_field_data(project = "zebby_tracking",
+                 google_drive_folder = "Zebby_tracking_field_data",
+                 overwrite = TRUE,
+                 update = FALSE)
+
+## Process detection data
+process_dets(db_name = db_name,
+             db_user = db_user,
+             db_password = db_password,
+             project = "Eswatini",
+             sensor_station_code = NULL,
+             tz = "Africa/Mbabane")
 # 
-# ## Process detection data
-# process_nodes(db_name = db_name,
-#               db_user = db_user,
-#               db_password = db_password,
-#               sensor_station_code = NULL,
-#               node_log_file = "/Users/tyson/Documents/academia/institutions/WUR/research/eswatini/field/Nodes/Node_deployment_log_Feb2022.xlsx",
-#               tz = "Africa/Mbabane",
-#               output_folder = "./Eswatini/data/processed")
-# 
-# ## Summarize detections and plot
-# summarize_dets(dets_data_file = "./Eswatini/data/processed/dets_filtered.Rdata",
-#                plot_type = "summary",
-#                interval = "day",
-#                summary_folder = "./Eswatini/data_summaries",
-#                plot_folder = "./Eswatini/plots")
-# 
-# ## Summarize detections and plot
-# summarize_nodes(node_data_file = "./Eswatini/data/processed/node_health.Rdata",
-#                 dets_data_file = "./Eswatini/data/processed/dets_filtered.Rdata",
-#                 plot_type = "summary",
-#                 interval = "day",
-#                 summary_folder = "./Eswatini/data_summaries",
-#                 plot_folder = "./Eswatini/plots")
+## Process detection data
+process_nodes(db_name = db_name,
+              db_user = db_user,
+              db_password = db_password,
+              sensor_station_code = NULL,
+              tz = "Africa/Mbabane")
+
+## Summarize detections and plot
+summarize_dets(dets_data_file = "./Eswatini/data/processed/dets_filtered.Rdata",
+               plot_type = "summary",
+               interval = "day",
+               summary_folder = "./Eswatini/data_summaries",
+               plot_folder = "./Eswatini/plots")
+
+## Summarize detections and plot
+summarize_nodes(node_data_file = "./Eswatini/data/processed/node_health.Rdata",
+                dets_data_file = "./Eswatini/data/processed/dets_filtered.Rdata",
+                plot_type = "summary",
+                interval = "day",
+                summary_folder = "./Eswatini/data_summaries",
+                plot_folder = "./Eswatini/plots")
 
 ## Prepare detection data to localize
 prepare_dets(processed_dets_file = "./Eswatini/data/processed/dets_filtered.Rdata",
@@ -57,3 +65,5 @@ localize(input_folder = "./Eswatini/data/processed/prepared/",
          grid_point_file = "./Eswatini/data/field_data/processed_field_data/grid_point_locations.RData",
          RSSI_model_file = "R/data/RSSI_log_distance_lm.RDS",
          reps = 100)
+
+## Home range analysis
