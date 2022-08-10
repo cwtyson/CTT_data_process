71398a47d808bd14bbd3eef3d448ee8df215ce6b
Tyson <cwtyson@gmail.com>
2022-08-10 05:36:55 +0200

    Update to new folder structure

--- R/functions/fetch_field_data_files.R
+++ R/functions/fetch_field_data_files.R
@@ -10,28 +10,29 @@ fetch_field_data <- function(google_drive_folder = as.character(),
                              overwrite = FALSE,
                              update = TRUE){
   
+  googledrive::drive_ls(path = google_drive_folder) %>%
+    drive_reveal("permissions")
+  
   ## Download all files and save to relevant project folder
   googledrive::drive_ls(path = google_drive_folder) %>% 
     split(.$id) %>% 
     purrr::walk(~googledrive::drive_download(.$id, 
-                                             path = here::here("project", project, "data/field", .$name), 
+                                             path = here::here("project", 
+                                                               project, 
+                                                               "data/field", .$name), 
                                              overwrite = overwrite))
   
   if(update == TRUE){
-
+    
     ## Update node log file
-    process_node_log(node_log_file = list.files(here::here("project", project,"data/field"), 
-                                                           pattern = "grid_points",
+    process_node_log(node_log_file = list.files(here::here("project", 
+                                                           project,
+                                                           "data/field"), 
+                                                pattern = "grid_points",
                                                 recursive = TRUE,
                                                 full.names = TRUE),
                      output_folder = here::here(paste0("project/",project,"/data/processed/field_data/")))
-                     
-
+    
   }
-
+  
 }
-
-
-
-
-
