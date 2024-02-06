66e8bfa744b60ea006b1d39a4a824b377ad1e69e
Chris Tyson <chris.tyson@wur.nl>
2024-02-06 08:14:07 +0100

    Create separate functions for compiling grid points for zebby and mousebird projects

--- R/functions/get_grid_points_fn_mousebird.R
+++ R/functions/get_grid_points_fn_mousebird.R
@@ -1,4 +1,5 @@
-get_grid_points_fn <- function(grid_points_folder){
+get_grid_points_fn <- function(grid_points_folder,
+                               crs){
   
   
   ## Get all grid point files
@@ -14,6 +15,7 @@ get_grid_points_fn <- function(grid_points_folder){
     ## Get grid point coordinates
     grid_points_f <- suppressWarnings(sf::read_sf(gp_f) %>% 
                                         sf::st_transform(crs) %>% 
+                                        janitor::clean_names() %>% 
                                         dplyr::transmute(grid_point = name,
                                                          gp_x = as.matrix((sf::st_coordinates(.data$geometry)), ncol = 2)[,1],
                                                          gp_y = as.matrix((sf::st_coordinates(.data$geometry)), ncol = 2)[,2]) %>% 
@@ -28,6 +30,12 @@ get_grid_points_fn <- function(grid_points_folder){
   grid_points <- grid_points %>% 
     distinct(grid_point,
              .keep_all = T)
+  
+  ## Rename grid points by removing "Gp "
+  grid_points$grid_point <- gsub(pattern = "Gp ",
+                                 replacement = "",
+                                 grid_points$grid_point)
+  
   
   return(grid_points)
 }
