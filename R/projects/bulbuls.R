ee29b2ed555065c0b70a841fc678da23c5f17576
Chris Tyson <tyson@ChrissMacStudio.wurnet.nl>
2024-04-11 12:16:58 +0200

    Update bulbul project scripts

--- R/projects/bulbuls.R
+++ R/projects/bulbuls.R
@@ -1,33 +1,63 @@
 ## Update localizaitons using multilateration method
 library(foreach)
 library(dplyr)
+library(geosphere)
 
-# ## Source functions
-# functions <- list.files("./R/functions",full.names = T,pattern="_fn")
-# source(functions)
-
-source("./R/functions/ml_update_localizations_fn.R")
-source("./R/functions/collect_raw_data_fn.R")
+## Source functions
+source("./R/functions/ml_update_localizations_fn_mousebird.R")
+source("./R/functions/collect_raw_data_fn_mousebird.R")
 source("./R/functions/ml_prepare_dets_error_fn.R")
-source("./R/functions/ml_localize_dets_error_fn.R")
+source("./R/functions/ml_localize_dets_error_fn_mousebird.R")
+source("./R/functions/get_grid_points_fn_mousebird.R")
 
-## Set up parallelization options
-cl <- parallel::makeForkCluster(8, outfile = "")
+cores<-parallel::detectCores()-3
+cl <- parallel::makeForkCluster(cores, outfile = "")
 doParallel::registerDoParallel(cl)
 
-## Define the tags to be processed- should be a vector
-bands <- readxl::read_xlsx("/Users/tyson/Library/CloudStorage/GoogleDrive-cwtyson@gmail.com/My Drive/Eswatini_field_data/2023/tag_logs/tag_log_20230909.xlsx") %>% 
-  janitor::clean_names() %>% 
-  filter(species == "DCBU") %>% 
+## Define the bird bands to be processed- should be a vector
+bird_bands <- readxl::read_xlsx("/Users/tyson/Library/CloudStorage/GoogleDrive-cwtyson@gmail.com/My Drive/Eswatini_field_data/2023/tag_logs/tag_log_20230909.xlsx") %>% 
+  mutate(year = format(lubridate::dmy(date), "%Y")) %>% 
+  filter(year == "2021") %>%
+  filter(species == "DCBU" ) %>% 
   pull(bird_band) %>% 
   unique()
 
-foreach(tag_f=bands,.packages=c("tidyverse","lubridate","readr","geosphere"),
-        .verbose = TRUE) %dopar%
-  { ml_update_localizations_fn(
+## Get maximum date for preparing data to account for sensor station uploads
+
+## Active sensor stations
+ss_ids <- c("39C9F709EC64","31517E791AAE", "98B773B8FE7C")
+
+max_dates <- data.frame()
+suppressWarnings(
+  for(ss_id in ss_ids){
+    
+    files <- list.files(paste0("/Volumes/ctt_data/files/Mouse Bird/",ss_id,"/raw"))
+    dates <- lapply(files, function(x) unlist(strsplit(x,split = "[.]")))
+    dates_df <- do.call(rbind, dates) %>% 
+      data.frame() %>% 
+      transmute(date= lubridate::ymd_hms(X2, tz="Africa/Mbabane"),
+                ss = ss_id,
+                max_date = max(date)) %>% 
+      filter(date == max_date)
+    max_dates <- bind_rows(max_dates, dates_df)
+    
+  }
+)
+## Keep oldest download date as filter
+ss_date_filter = min(max_dates$max_date)
+
+## Run in termainal
+## OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES Rscript ./R/projects/mousebirds.R
+
+# bird_bands = "4A94970"
+
+foreach(band_f = bird_bands,
+        .packages=c("tidyverse","lubridate","readr","geosphere"),
+        .verbose = FALSE) %dopar%
+  { ml_update_localizations_fn_mousebird(
     
     ## Database credentials
-    db_name = "tyson",
+    db_name = "mousebird",
     
     ## Tag value is defined in foreach function
     band_f = band_f,
@@ -42,13 +72,16 @@ foreach(tag_f=bands,.packages=c("tidyverse","lubridate","readr","geosphere"),
     grid_points_folder = "/Users/tyson/Library/CloudStorage/GoogleDrive-cwtyson@gmail.com/My Drive/Eswatini_field_data/2023/grid_points/",
     
     ## Folder where the data should be saved. A new folder will be created for each tag
-    output_folder =   "/Volumes/mac_mini_storage/academia/research/Eswatini/data/processed_detections/bulbuls",
+    output_folder =   "/Volumes/data_bases/bulbuls/processed_detections/",
     
     ## Location of log-linear model RSSI~distance output
     log_dist_RSSI_mdl = "/Users/tyson/Library/CloudStorage/GoogleDrive-cwtyson@gmail.com/My Drive/Eswatini_field_data/2023/RSSI_log_distance_lm_Eswatini.RDS",
     
     ## Time zone
     tz = "Africa/Mbabane",
+    
+    ## Sensor station date filter
+    ss_filter = ss_date_filter,
     
     ## Projected CRS to use
     crs = 22291,
